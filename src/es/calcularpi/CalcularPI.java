d9c14773704068d6d9909fcc3df2563a5421c21f
Alumno_Distancia <Alumno_Distancia@DESKTOP-SAL77AL>
2022-12-16 13:42:15 +0100

    v4.-aquí escribiremos las acciones realizadas para la refactorización

--- src/es/calcularpi/CalcularPI.java
+++ src/es/calcularpi/CalcularPI.java
@@ -7,17 +7,22 @@ import java.math.MathContext;
 public class CalcularPI {
 
      public static void main(String[] args) {
-         //Primero hacemos clic derecho sobre el NUMERO 10 y vamos a la opcion refactor.
+         //Primero hacemos clic derecho sobre el codigo(10) y vamos a la opcion refactor.
         //En refactor vamos a la opcion introduce y luego a constant.
         //Seleccionamos el nombre de la constante(NUM_DIG),su tipo de encapsulacion(public) y seleccionamos la opcion de reemplazar sus ocurrencias.
         MathContext mc = new MathContext(NUM_DIG);         
-        //meter en el método compute
+        compute(mc);
+    }
+
+    public static void compute(MathContext mc) {
+        //Primero hacemos clic derecho sobre el codigo y vamos a la opcion refactor.
+        //En refactor vamos a la opcion introduce y luego a method.
+        //Seleccionamos el nombre del metodo(compute) y su tipo de encapsulacion(public).
         BigDecimal pi = new BigDecimal(0);
         BigDecimal limit = new BigDecimal(1).movePointLeft(NUM_DIG);
         boolean stop = false;
         for (int k = 0; !stop; k++) {
-            BigDecimal piK = piFunction(k, mc);
-            //meter en el método piFunction                
+            BigDecimal piK = piFunction(k, mc);               
             pi = pi.add(piK);
             if (piK.compareTo(limit) < 0) {
                 stop = true;
